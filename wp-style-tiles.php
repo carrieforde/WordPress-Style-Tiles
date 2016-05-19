73372206e2da995e8564f2c17d289fa1d06cc3f9
carrieforde <carrie@carrieforde.com>
2016-05-18 20:20:33 -0700

    Define plugin constants and set up theme styles and scripts

--- wp-style-tiles.php
+++ wp-style-tiles.php
@@ -10,3 +10,28 @@
  * Domain Path: /languages
  * @package wp-style-tiles
  */
+
+define( 'WP_STYLE_TILES_VERSION', '1.0.0' );
+define( 'WP_STYLE_TILES_PATH', plugin_dir_path( __FILE__ ) );
+define( 'WP_STYLE_TILES_URL', plugin_dir_url( __FILE__ ) );
+
+add_action( 'wp_enqueue_scripts', 'wpst_styles_and_scripts' );
+/**
+ * Load plugin scripts and styles.
+ */
+function wpst_styles_and_scripts() {
+
+	wp_enqueue_style(
+		'wpst-styles',
+		WP_STYLE_TILES_URL . 'wp-style-tiles.css',
+		array(),
+		WP_STYLE_TILES_VERSION
+	);
+
+	wp_enqueue_script(
+		'wpst-scripts',
+		WP_STYLE_TILES_URL . 'js/wp-style-tiles.js',
+		array( 'jquery' ),
+		WP_STYLE_TILES_VERSION
+	);
+}
