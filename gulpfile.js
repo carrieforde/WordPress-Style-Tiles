84b0106af6ff15cae6a024c1ce85bba760bb6845
carrieforde <carrie@carrieforde.com>
2016-08-11 17:43:51 -0700

    Try to set up browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var autoprefixer  = require( 'autoprefixer' );
 var bourbon       = require( 'bourbon' );
+var browserSync   = require( 'browser-sync' );
 var del           = require( 'del' );
 var gulp          = require( 'gulp' );
 var mqpacker      = require( 'css-mqpacker' );
@@ -9,6 +10,10 @@ var postcss       = require( 'gulp-postcss' );
 var rename        = require( 'gulp-rename' );
 var sass          = require( 'gulp-sass' );
 var sourcemaps    = require( 'gulp-sourcemaps' );
+
+var paths = {
+	sass: 'assets/sass/**/*.scss',
+};
 
 gulp.task( 'clean:styles', function() {
 	return del(['wp-style-tiles.css', 'wp-style-tiles.min.css'])
@@ -24,7 +29,7 @@ gulp.task( 'sass', function() {
 				require('bourbon-neat').includePaths
 			]
 		}))
-		.pipe( sass().on('error', sass.logError))
+		.pipe(sass().on('error', sass.logError))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('./assets/css'));
 });
@@ -34,9 +39,25 @@ gulp.task( 'postcss', ['clean:styles'], function() {
 		autoprefixer({browsers: ['last 2 version']}),
 		mqpacker({sort: true}),
 	];
+
 	return gulp.src('./assets/css/*.css')
 		.pipe(postcss(processors))
-		.pipe(gulp.dest('./'));
+		.pipe(gulp.dest('./'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task( 'styles', ['sass', 'postcss'] );
+
+gulp.task( 'watch', function() {
+
+	browserSync({
+		open: false,
+		injectChanges: true,
+		proxy: "local.wordpress.dev",
+		watchOptions: {
+			debounceDelay: 1000
+		}
+	});
+
+	gulp.watch(paths.sass, ['styles']);
+});
